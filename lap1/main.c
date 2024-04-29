8b79854913abcb18cfd13b4da544889b7b490271
tobysimpson <tobygeneva@hotmail.com>
2024-04-29 16:14:18 +0200

    Initial Commit

---
+++ lap1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lap1
+//
+//  Created by toby on 29.04.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
