f3fcf8442dac504675c4837476b3d3b6538b8144
tobysimpson <tobygeneva@hotmail.com>
2024-04-29 16:59:48 +0200

    runs kernel

---
+++ lap1/ocl.h
@@ -0,0 +1,12 @@
+//
+//  ocl.h
+//  lap1
+//
+//  Created by toby on 29.04.24.
+//
+
+#ifndef ocl_h
+#define ocl_h
+
+
+#endif /* ocl_h */
---
+++ lap1/prg.cl
@@ -0,0 +1,8 @@
+//
+//  prg.c
+//  lap1
+//
+//  Created by toby on 29.04.24.
+//
+
+#include <stdio.h>
